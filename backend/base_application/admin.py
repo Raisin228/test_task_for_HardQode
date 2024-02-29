c5a6d6a8a67861ccfd1baf57bc6e64df0c3096b3
Bogdan <bogdanatrosenko@gmail.com>
2024-02-29 21:29:50 +0300

    I have completely completed the first block of tasks + fixed the database fixture

--- backend/base_application/admin.py
+++ backend/base_application/admin.py
@@ -1,10 +1,40 @@
 from django.contrib import admin
 
-from .models import Product, Lesson, GroupStudent, Group
+from .models import Product, Lesson, Group, GroupStudent
+
+
+class AbstractClass(admin.ModelAdmin):
+    """В этот класс вынесены все методы которые повторяются в остальных моделях
+        Стараемся соблюдать принцип DRY!"""
+
+    def get_readonly_fields(self, request, obj=None):
+        """Возвращает поля, которые доступны только на чтение"""
+        if obj:
+            return self.readonly_fields
+        return ()
+
+    def has_change_permission(self, request, obj=None) -> bool:
+        """Выдаём возможность редактировать объект только его автору"""
+        if obj is not None:
+            product_id = obj.product_id
+            try:
+                product = Product.objects.get(pk=product_id)
+                return request.user == product.author
+            except:
+                return False
+        return False
+
+    def has_delete_permission(self, request, obj=None) -> bool:
+        """Проверяет разрешение на удаление объекта"""
+        if obj is not None:
+            product = obj.product_id
+            if Product.objects.get(id=product).author == request.user:
+                return True
+        return False
 
 
 @admin.register(Product)
-class ProductAdminView(admin.ModelAdmin):
+class ProductAdminView(AbstractClass):
     """Возможность редактирования продуктов из админки"""
     list_display = ('title', 'author', 'data_start')
     readonly_fields = ('data_start', 'author')
@@ -17,62 +47,55 @@ class ProductAdminView(admin.ModelAdmin):
             form.base_fields['author'].queryset = form.base_fields['author'].queryset.filter(id=request.user.id)
         return form
 
-    def get_readonly_fields(self, request, obj=None):
-        """Возвращает поля, которые доступны только на чтение"""
-        if obj:
-            return self.readonly_fields
-        return ()
-
     def has_change_permission(self, request, obj=None):
         """Выдаём возможность редактировать продукт только его автору"""
         if obj is not None and request.user == obj.author:
             return True
         return False
 
+    def has_delete_permission(self, request, obj=None) -> bool:
+        """Проверяет разрешение на удаление объекта"""
+        # нужно переопределить т.к идёт другой запрос на проверку
+        if obj is not None and request.user == obj.author:
+            return True
+        return False
+
 
 @admin.register(Lesson)
-class LessonAdminView(admin.ModelAdmin):
+class LessonAdminView(AbstractClass):
     """Взаимодействие с уроками через админку"""
     list_display = ('title', 'link_to_video')
     readonly_fields = ('product',)
     fields = ('title', 'product', 'link_to_video')
 
     def get_form(self, request, obj=None, **kwargs):
-        """Получаем форму и фильтруем выбор курсов.
-        Сделано потому что автор может выкладывать видео только к своему курсу"""
+        """Получаем форму и фильтруем. Оставляем только те данные которые принадлежат этому автору"""
         form = super().get_form(request, obj, **kwargs)
         if obj is None:
             form.base_fields['product'].queryset = form.base_fields['product'].queryset.filter(
                 author_id=request.user.id)
         return form
 
-    def get_readonly_fields(self, request, obj=None):
-        """Возвращает поля которые доступны только на чтение"""
-        if not obj:
-            return ()
-        return self.readonly_fields
 
-    def has_change_permission(self, request, obj=None):
-        """Выдаём возможность редактировать урок только его автору"""
-        if obj is not None:
-            product_id = obj.product_id
-            try:
-                product = Product.objects.get(pk=product_id)
-                return request.user == product.author
-            except:
-                return False
-        return False
+class GroupStudentInline(admin.TabularInline):
+    """Добавил инлайн форму для отображения списка студентов принадлежащих группе"""
+    model = GroupStudent
+    extra = 1
 
 
 @admin.register(Group)
-class GroupAdminView(admin.ModelAdmin):
+class GroupAdminView(AbstractClass):
     """Все группы запущенные на платформу"""
+    inlines = (GroupStudentInline,)
+
     list_display = ('title', 'product')
     readonly_fields = ('product',)
     fields = ('title', 'product')
 
-    def get_readonly_fields(self, request, obj=None):
-        """Возвращает поля которые доступны только на чтение"""
-        if not obj:
-            return ()
-        return self.readonly_fields
+    def get_form(self, request, obj=None, **kwargs):
+        """Получаем форму и фильтруем. Оставляем только те данные которые принадлежат этому автору"""
+        form = super().get_form(request, obj, **kwargs)
+        if obj is None:
+            form.base_fields['product'].queryset = form.base_fields['product'].queryset.filter(
+                author_id=request.user.id)
+        return form
